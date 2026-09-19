fdc165d115dc0ec3c0b6ace7dc556d09dd970921
tanban00018173 <tanban00018173@gmail.com>
2026-09-19 11:01:55 +0800

    Announce first Yilan station with physical activities preparing

--- moomoo/sw.js
+++ moomoo/sw.js
@@ -1,4 +1,4 @@
-const CACHE='moomoo-verse-20260919-v1';
+const CACHE='moomoo-verse-20260919-v1-1';
 const FILES=['./','./index.html','./style.css','./app.js','./state.js','./assets/moo-mark.svg','./assets/moomoo-master.png','./assets/yilan-hero.webp','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('moomoo-verse-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
